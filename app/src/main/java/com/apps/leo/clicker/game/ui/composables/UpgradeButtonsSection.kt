568f20e4890d4be770ba13592016d2a5c705b7d2
lbelyakov <lbelyakov@cochlear.com>
2024-03-04 18:50:35 +1100

    Improved Upgrade Buttons

--- app/src/main/java/com/apps/leo/clicker/game/ui/composables/UpgradeButtonsSection.kt
+++ app/src/main/java/com/apps/leo/clicker/game/ui/composables/UpgradeButtonsSection.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -27,11 +28,13 @@ import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.rememberTextMeasurer
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -65,7 +68,20 @@ fun UpgradeButton(
 ) {
     var isPressed by remember { mutableStateOf(false) }
     val scale = remember { Animatable(initialValue = 1f) }
+
     val borderStroke = BorderStroke(4.dp, Color.Blue)
+
+    val textMeasurer = rememberTextMeasurer()
+    val priceWidthLimit = remember {
+        textMeasurer.measure(
+            text = "999.9K$",
+            style = TextStyle(
+                color = Color.Black,
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold,
+            )
+        ).size.width
+    }
 
     LaunchedEffect(isPressed) {
         if (isPressed) {
@@ -131,16 +147,22 @@ fun UpgradeButton(
             )
         }
         Box(
+            contentAlignment = Alignment.Center,
             modifier = Modifier
                 .align(Alignment.CenterVertically)
                 .border(border = borderStroke, shape = RoundedCornerShape(12.dp))
                 .background(color = Color.Green, shape = RoundedCornerShape(12.dp))
-                .padding( // todo make all buttons of the same size
-                    vertical = 8.dp,
-                    horizontal = 12.dp
-                )
+                .padding(vertical = 8.dp, horizontal = 12.dp)
+                .width(with(LocalDensity.current) { priceWidthLimit.toDp() })
         ) {
-            Text(text = state.priceText)
+            Text(
+                text = state.priceText,
+                style = TextStyle(
+                    color = Color.Black,
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold,
+                )
+            )
         }
     }
 }
